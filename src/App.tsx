5b157a0e3282a4451c43aed1fa71f6b275eeaf82
Boltach228 <121123133+AddBlockk@users.noreply.github.com>
2024-06-06 15:52:45 +0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,17 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext";
 
 function App() {
-  type ProtectedRouteProps = PropsWithChildren<{}>;
+  // type ProtectedRouteProps = PropsWithChildren<{}>;
 
-  const ProtectedRoute: FC<ProtectedRouteProps> = ({ children }) => {
-    const { currentUser } = useContext(AuthContext);
+  // const ProtectedRoute: FC<ProtectedRouteProps> = ({ children }) => {
+  //   const { currentUser } = useContext(AuthContext);
 
-    if (!currentUser) {
-      return <Navigate to="/login" />;
-    }
+  //   if (!currentUser) {
+  //     return <Navigate to="/login" />;
+  //   }
 
-    return <>{children}</>;
-  };
+  //   return <>{children}</>;
+  // };
 
   return (
     <BrowserRouter>
@@ -27,13 +27,13 @@ function App() {
           <Route
             index
             element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
+              // <ProtectedRoute>
+              <Home />
+              // </ProtectedRoute>
             }
           />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
+          {/* <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
